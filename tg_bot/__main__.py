7c11541caed603e410cee912ec3a3156a10be2b5
Abin Paul Zachariah <39844254+K1DD0SC4LLM3NOOB@users.noreply.github.com>
2018-07-20 01:58:57 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,15 +18,11 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! . I'm So Glad that you decided to use me.. 😉 If you have doubts for using me  just use /help command.... if you still doubts after using that command you can ask freely at @mariebotmalayalamsupport ❤️
+Hi {}, my name is {}! . I'm So Glad that you decided to use me.. 😉 use /help command if you any doubts about how to use me or you can freely ask at @mariebotmalayalamsupport 😊
 
-I'm a group manager bot maintained by [this wonderful person](tg://user?id={}). I'm built in python3, using the \
+I'm a group manager bot maintained by [This Sweety](tg://user?id={}). I'm built in python3, using the \
 python-telegram-bot library, and am fully opensource - you can find what makes me tick \
-[here](github.com/Achuzz/tgbot)!
-
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+[here](github.com/achuzz/tgbot)
 
 You can find the list of available commands with /help.
 
@@ -52,10 +48,8 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/SonOfLars) to get me to where I am now, and every donation helps \
-motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
-(see his bio!). He's just a poor student, so every little helps!
-There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
+It took lots of work for [my creator](t.me/AbinPaulZachariah) to get me to where I am now, and every donation helps \
+motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer . He's just a poor student, so every little helps! For Donations Contact Him [Here](t.me/K1DD0SC4LLM3NOOB)."""
 
 IMPORTED = {}
 MIGRATEABLE = []
